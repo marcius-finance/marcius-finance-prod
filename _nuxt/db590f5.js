8e8d2a8a8954321a2b92b9665f960c5d3c674252
Nikita Marcius <nikita.marcius@gmail.com>
2022-03-09 16:59:54 +0300

    +

---
+++ _nuxt/db590f5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{679:function(e,n,t){"use strict";t.r(n);var c={},l=t(1),component=Object(l.a)(c,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("c-page",[e._v("wars")])}),[],!1,null,null,null);n.default=component.exports}}]);
